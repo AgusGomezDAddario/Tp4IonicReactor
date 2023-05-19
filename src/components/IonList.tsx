f88a66417848d518c521b178cc568d1e364c5743
Agustin Gomez D'Addario <gomezdaddarioagus@gmail.com>
2023-05-18 23:14:20 -0300

    Elimina personas

--- src/components/IonList.tsx
+++ src/components/IonList.tsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { IonItem, IonLabel, IonList, IonModal } from '@ionic/react';
-import './ExploreContainer.css';
-import { IonIcon, IonItemOption, IonItemOptions, IonItemSliding,  IonReorder, IonReorderGroup, ItemReorderEventDetail } from '@ionic/react';
+import {
+  IonItem,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonIcon,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonReorder,
+  IonReorderGroup,
+  ItemReorderEventDetail,
+} from '@ionic/react';
 import { trash } from 'ionicons/icons';
-
 
 interface ExampleProps {
   setShowModal: Function;
@@ -11,11 +20,8 @@ interface ExampleProps {
   persona: any[];
 }
 
-const Example = (
-  { setShowModal, setPersona, persona }: ExampleProps) => {
-
+const Example = ({ setShowModal, setPersona, persona }: ExampleProps) => {
   const [data, setData] = useState<any[]>([]);
-
 
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=10')
@@ -23,7 +29,7 @@ const Example = (
       .then(data => {
         console.log(data.results);
         setData(data.results);
-        console.log(data.results[0].picture.thumbnail)
+        console.log(data.results[0].picture.thumbnail);
       })
       .catch(error => {
         console.log('Error:', error);
@@ -31,43 +37,50 @@ const Example = (
   }, []);
 
   function handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
-    // The `from` and `to` properties contain the index of the item
-    // when the drag started and ended, respectively
     console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
-
-    // Finish the reorder and position the item in the DOM based on
-    // where the gesture ended. This method can also be called directly
-    // by the reorder group
     event.detail.complete();
+  }
+
+  function handleDeleteItem(index: number) {
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
   }
 
   return (
     <IonList>
       <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
-      {data.map((persona, index) => (
-        <IonItemSliding>
-          <IonItem className="item" key={index} button
-            onClick={() => {
-              setShowModal(true);
-              setPersona(persona);
-            }}>
-            <IonLabel className='ionLabel'>
-              <img src={persona.picture.thumbnail} alt="" />
-              <p className='nombres'>{persona.name?.first + ", " + persona.name?.last}</p>
-            </IonLabel>
-            <IonReorder slot="end"></IonReorder>
-          </IonItem>
-          <IonItemOptions side="end">
-            <IonItemOption color="danger">
-              <IonIcon slot="icon-only" icon={trash}></IonIcon>
-            </IonItemOption>
-          </IonItemOptions>
-        </IonItemSliding>
-      ))
-      }
+        {data.map((persona, index) => (
+          <IonItemSliding key={index}>
+            <IonItem
+              className="item"
+              button
+              onClick={() => {
+                setShowModal(true);
+                setPersona(persona);
+              }}
+            >
+              <IonLabel className="ionLabel">
+                <img src={persona.picture.thumbnail} alt="" />
+                <p className="nombres">
+                  {persona.name?.first + ', ' + persona.name?.last}
+                </p>
+              </IonLabel>
+              <IonReorder slot="end" />
+            </IonItem>
+            <IonItemOptions side="end">
+              <IonItemOption
+                onClick={() => handleDeleteItem(index)}
+                color="danger"
+              >
+                <IonIcon slot="icon-only" icon={trash} />
+              </IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
+        ))}
       </IonReorderGroup>
-    </IonList >
+    </IonList>
   );
-}
+};
 
 export default Example;
